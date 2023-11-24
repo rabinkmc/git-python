9111616fe90a68add0de401e6bb691407272dff4
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-24 16:18:46 +0545

    create git object

--- app/main.py
+++ app/main.py
@@ -1,12 +1,30 @@
 import sys
 import os
 import zlib
+import hashlib
 
 
 def cat_file(blob_sha):
     compressed_file = f".git/objects/{blob_sha[0:2]}/{blob_sha[2:]}"
     _, file = decompress(compressed_file).split(b"\x00")
     print(file.decode(), end="")
+
+
+def hash_object(filename):
+    fr = open(filename, "r")
+    file_content = fr.read()
+    fr.close()
+    content = f"blob {len(file_content)}\x00{file_content}".encode()
+    checksum = hashlib.sha1(content).hexdigest()
+    dir = checksum[:2]
+    file_hash = checksum[2:]
+    content = zlib.compress(content)
+    if not os.path.isdir(f".git/objects/{dir}"):
+        os.mkdir(f".git/objects/{dir}")
+    f = open(f".git/objects/{dir}/{file_hash}", "wb")
+    f.write(content)
+    f.close()
+    print(checksum, end="")
 
 
 def decompress(compressed_file):
@@ -28,10 +46,14 @@ def main():
     command = sys.argv[1]
     if command == "init":
         init()
-    if command == "cat-file":
-        option = sys.argv[2]
+    elif command == "cat-file":
+        _ = sys.argv[2]
         value = sys.argv[3]
         cat_file(value)
+    elif command == "hash-object":
+        _ = sys.argv[2]
+        filename = sys.argv[3]
+        hash_object(filename)
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
